dd826eca4426dab28fc44fc44938e5d592ea7554
thanh toan <5442654@gmail.com>
2022-04-08 14:43:48 +0700

    UI frmXe

--- src/app/FrameXe.java
+++ src/app/FrameXe.java
@@ -1,23 +1,27 @@
 package app;
 
 import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class FrameXe extends JFrame{
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+
 	public FrameXe() {
 		setSize(1345, 705);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,109 +30,257 @@ public class FrameXe extends JFrame{
 		getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		//panel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		panel.setBackground(new Color(166, 169, 248));
-		panel.setBounds(0, 0, 506, 668);
+		panel.setBounds(0, 0, 796, 241);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblMaXe = new JLabel("Mã xe:");
-		lblMaXe.setBounds(10, 23, 46, 14);
+		lblMaXe.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblMaXe.setBounds(10, 63, 102, 14);
 		panel.add(lblMaXe);
 		
-		textField = new JTextField();
-		textField.setBounds(66, 20, 86, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtMaXe = new JTextField();
+		txtMaXe.setBounds(103, 58, 214, 28);
+		panel.add(txtMaXe);
+		txtMaXe.setColumns(10);
 		
 		JLabel lblTenXe = new JLabel("Tên xe:");
-		lblTenXe.setBounds(10, 61, 46, 14);
+		lblTenXe.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTenXe.setBounds(10, 101, 102, 14);
 		panel.add(lblTenXe);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(66, 58, 86, 20);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		txtTenXe = new JTextField();
+		txtTenXe.setBounds(103, 96, 214, 28);
+		panel.add(txtTenXe);
+		txtTenXe.setColumns(10);
 		
 		JLabel lblMauXe = new JLabel("Màu xe:");
-		lblMauXe.setBounds(10, 95, 46, 14);
+		lblMauXe.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblMauXe.setBounds(10, 135, 102, 14);
 		panel.add(lblMauXe);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(66, 92, 86, 20);
-		panel.add(comboBox);
+		JComboBox cboMauXe = new JComboBox();
+		cboMauXe.setBounds(103, 130, 214, 28);
+		panel.add(cboMauXe);
 		
 		JLabel lblSoKhung = new JLabel("Số khung:");
-		lblSoKhung.setBounds(10, 126, 46, 14);
+		lblSoKhung.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblSoKhung.setBounds(10, 170, 102, 23);
 		panel.add(lblSoKhung);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(66, 123, 86, 20);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
+		txtSoKhung = new JTextField();
+		txtSoKhung.setBounds(103, 165, 214, 28);
+		panel.add(txtSoKhung);
+		txtSoKhung.setColumns(10);
 		
 		JLabel lblSoMay = new JLabel("Số máy:");
-		lblSoMay.setBounds(10, 151, 46, 14);
+		lblSoMay.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblSoMay.setBounds(10, 207, 91, 23);
 		panel.add(lblSoMay);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(66, 148, 86, 20);
-		panel.add(textField_3);
-		textField_3.setColumns(10);
+		txtSoMay = new JTextField();
+		txtSoMay.setBounds(103, 202, 214, 28);
+		panel.add(txtSoMay);
+		txtSoMay.setColumns(10);
 		
 		JLabel lblNhaCungCap = new JLabel("Nhà cung cấp:");
-		lblNhaCungCap.setBounds(173, 23, 46, 14);
+		lblNhaCungCap.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNhaCungCap.setBounds(369, 63, 128, 23);
 		panel.add(lblNhaCungCap);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(229, 20, 86, 20);
-		panel.add(textField_4);
-		textField_4.setColumns(10);
+		txtNhaCungCap = new JTextField();
+		txtNhaCungCap.setBounds(507, 58, 214, 28);
+		panel.add(txtNhaCungCap);
+		txtNhaCungCap.setColumns(10);
 		
-		JLabel lblHangSanXuat = new JLabel("Hãng sản xuất");
-		lblHangSanXuat.setBounds(173, 61, 46, 14);
+		JLabel lblHangSanXuat = new JLabel("Hãng sản xuất:");
+		lblHangSanXuat.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblHangSanXuat.setBounds(369, 101, 128, 23);
 		panel.add(lblHangSanXuat);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(229, 58, 86, 20);
-		panel.add(textField_5);
-		textField_5.setColumns(10);
+		txtHangSanXuat = new JTextField();
+		txtHangSanXuat.setBounds(507, 96, 214, 28);
+		panel.add(txtHangSanXuat);
+		txtHangSanXuat.setColumns(10);
 		
 		JLabel lblLoaiXe = new JLabel("Loại xe:");
-		lblLoaiXe.setBounds(173, 95, 46, 14);
+		lblLoaiXe.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblLoaiXe.setBounds(369, 135, 128, 23);
 		panel.add(lblLoaiXe);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(229, 92, 86, 20);
-		panel.add(comboBox_1);
+		JComboBox cboLoaiXe = new JComboBox();
+		cboLoaiXe.setBounds(507, 130, 214, 28);
+		panel.add(cboLoaiXe);
 		
 		JLabel lblGiaNhap = new JLabel("Giá nhập:");
-		lblGiaNhap.setBounds(173, 126, 46, 14);
+		lblGiaNhap.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblGiaNhap.setBounds(369, 170, 128, 23);
 		panel.add(lblGiaNhap);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(229, 123, 86, 20);
-		panel.add(textField_6);
-		textField_6.setColumns(10);
+		txtGiaNhap = new JTextField();
+		txtGiaNhap.setBounds(507, 165, 214, 28);
+		panel.add(txtGiaNhap);
+		txtGiaNhap.setColumns(10);
 		
 		JLabel lblTrangThai = new JLabel("Trạng thái:");
-		lblTrangThai.setBounds(173, 151, 46, 14);
+		lblTrangThai.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTrangThai.setBounds(369, 207, 128, 23);
 		panel.add(lblTrangThai);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(229, 148, 86, 20);
-		panel.add(comboBox_2);
+		JComboBox cboTrangThai = new JComboBox();
+		cboTrangThai.setBounds(507, 202, 214, 28);
+		panel.add(cboTrangThai);
 		
+		JLabel lblTimKiem = new JLabel("Tìm kiếm:");
+		lblTimKiem.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTimKiem.setBounds(213, 22, 91, 14);
+		panel.add(lblTimKiem);
 		
+		txtTimKiem = new JTextField();
+		txtTimKiem.setBounds(314, 17, 270, 28);
+		panel.add(txtTimKiem);
+		txtTimKiem.setColumns(10);
 		
-	}
+		JButton btnTimKH = new FixButton("Tìm");
+		btnTimKH.setIcon(new ImageIcon("image\\timkiem.png"));
 	
-	public JPanel createPanelXe() {
-		JPanel pnlContentPane = new JPanel();
-		pnlContentPane.setBackground(Color.WHITE);
-		setContentPane(pnlContentPane);
-		return pnlContentPane;
+		
+		btnTimKH.setForeground(Color.WHITE);
+		btnTimKH.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnTimKH.setBackground(new Color(107,96,236));
+		btnTimKH.setBounds(605, 15, 99, 32);
+		panel.add(btnTimKH);
+		
+		
+		JPanel pChucNang = new JPanel();
+		pChucNang.setBounds(839, 11, 480, 86);
+		pChucNang.setBackground(new Color(166, 169, 248));
+		pChucNang.setBorder(new LineBorder(Color.BLACK));
+		pChucNang.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1, true), "Chức năng",
+				TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		getContentPane().add(pChucNang);
+		
+		JButton btnThemKH = new FixButton("Thêm");
+		
+		btnThemKH.setIcon(new ImageIcon("image\\them.png"));
+		
+		btnThemKH.setForeground(Color.WHITE);
+		btnThemKH.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnThemKH.setBackground(new Color(107,96,236));
+		btnThemKH.setBounds(0, 336, 115, 49);
+		pChucNang.add(btnThemKH);
+		
+		JButton btnXoaKH = new FixButton("Xóa");
+		btnXoaKH.setIcon(new ImageIcon("image\\xoa.png"));
+		btnXoaKH.setForeground(Color.WHITE);
+		btnXoaKH.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnXoaKH.setBackground(new Color(107,96,236));
+		btnXoaKH.setBounds(125, 336, 109, 49);
+		pChucNang.add(btnXoaKH);
+		
+		JButton btnSuaKH = new FixButton("Sửa");
+	
+		btnSuaKH.setIcon(new ImageIcon("image\\capnhat.png"));
+		btnSuaKH.setForeground(Color.WHITE);
+		btnSuaKH.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnSuaKH.setBackground(new Color(107,96,236));
+		btnSuaKH.setBounds(244, 336, 109, 49);
+		pChucNang.add(btnSuaKH);
+		
+		JButton btnLamMoiKH = new FixButton("Làm mới");
+		btnLamMoiKH.setIcon(new ImageIcon("image\\lammoi.png"));
+		btnLamMoiKH.setForeground(Color.WHITE);
+		btnLamMoiKH.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnLamMoiKH.setBackground(new Color(107,96,236));
+		btnLamMoiKH.setBounds(366, 336, 138, 49);
+		pChucNang.add(btnLamMoiKH);
+		
+		JPanel pSapXep = new JPanel();
+		pSapXep.setLayout(null);
+		pSapXep.setBounds(839, 124, 466, 112);
+		pSapXep.setBackground(new Color(166, 169, 248));
+		pSapXep.setBorder(new LineBorder(Color.BLACK));
+		pSapXep.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1, true), "Sắp xếp",
+				TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		getContentPane().add(pSapXep);
+		
+		JLabel lblSapXep = new JLabel("Loại sắp xếp:");
+		lblSapXep.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblSapXep.setBounds(74, 30, 108, 23);
+		pSapXep.add(lblSapXep);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(192, 25, 214, 28);
+		pSapXep.add(comboBox);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 274, 1295, 325);
+		getContentPane().add(scrollPane);
+		
+		tableXe = new JTable(){
+			public Component prepareRenderer(TableCellRenderer rederer, int row, int column) {
+				Component c = super.prepareRenderer(rederer, row, column);
+				Color color1 = new Color(220,220,220);
+				Color color2 = Color.WHITE;
+				if (!c.getBackground().equals(getSelectionBackground())) {
+					Color coleur = (row % 2 == 0 ? color1 : color2);
+					c.setBackground(coleur);
+					coleur=null;
+				}
+				return c;
+			}
+		};
+		tableXe.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		tableXe.setGridColor(getBackground());
+		tableXe.setRowHeight(tableXe.getRowHeight() + 20);
+		tableXe.setSelectionBackground(new Color(166, 169, 248));
+		
+		JTableHeader tableHeader = tableXe.getTableHeader();
+		tableHeader.setBackground(new Color(79, 12, 132));
+		tableHeader.setForeground(Color.WHITE);
+		tableHeader.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+		tableXe.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tableXe.setModel(new DefaultTableModel(
+				new Object[][] {
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+					{null, null, null, null, null, null, null, null, null, null},
+				},
+				new String[] {
+					"Mã xe", "Tên xe", "Màu xe", "Số khung", "Số máy", "Nhà cung cấp", "Hãng sản xuất", "Loại xe", "Giá", "Trạng thái"
+				}
+			) {
+				boolean[] columnEditables = new boolean[] {
+					false, false, false, false, false, false, false, false, false, false
+				};
+				public boolean isCellEditable(int row, int column) {
+					return columnEditables[column];
+				}
+			});
+		
+		scrollPane.setViewportView(tableXe);
+		
+	
+		
 	}
+	private JTextField txtMaXe;
+	private JTextField txtTenXe;
+	private JTextField txtSoKhung;
+	private JTextField txtSoMay;
+	private JTextField txtNhaCungCap;
+	private JTextField txtHangSanXuat;
+	private JTextField txtGiaNhap;	
+	private JTextField txtTimKiem;
+	private JTable tableXe;
+
 	
 	public static void main(String[] args) {
 		new FrameXe().setVisible(true);
