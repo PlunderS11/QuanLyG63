80e6779da30d82de568dcf8bf3351f9fdec5185c
BuiPhucNguyen <pnguyen.93.012@gmail.com>
2022-04-19 00:27:32 +0700

    su kien frmKhachHang chua rang buoc

--- src/app/FrameBatDau.java
+++ src/app/FrameBatDau.java
@@ -7,7 +7,7 @@ public class FrameBatDau {
 		sc.setVisible(true);
 		try {
 			for (int row = 0; row <=100; row++) {
-                Thread.sleep(50);
+                Thread.sleep(10);
                 sc.loadingNumber.setText(Integer.toString(row)+"%");
                 sc.loading.setValue(row);
                 if (row == 100) {               
---
+++ src/entity/KhachHang.java
@@ -0,0 +1,99 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class KhachHang implements Serializable{
+	private String maKH;
+	private String tenKH;
+	private Date ngaySinh;
+	private String diaChi;
+	private String sDT;
+	private String cCCD;
+	private boolean gioiTinh;
+	public String getMaKH() {
+		return maKH;
+	}
+	public void setMaKH(String maKH) {
+		this.maKH = maKH;
+	}
+	public String getTenKH() {
+		return tenKH;
+	}
+	public void setTenKH(String tenKH) {
+		this.tenKH = tenKH;
+	}
+	public Date getNgaySinh() {
+		return ngaySinh;
+	}
+	public void setNgaySinh(Date ngaySinh) {
+		this.ngaySinh = ngaySinh;
+	}
+	public String getDiaChi() {
+		return diaChi;
+	}
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+	public String getsDT() {
+		return sDT;
+	}
+	public void setsDT(String sDT) {
+		this.sDT = sDT;
+	}
+	public String getcCCD() {
+		return cCCD;
+	}
+	public void setcCCD(String cCCD) {
+		this.cCCD = cCCD;
+	}
+	public boolean isGioiTinh() {
+		return gioiTinh;
+	}
+	public void setGioiTinh(boolean gioiTinh) {
+		this.gioiTinh = gioiTinh;
+	}
+	public KhachHang(String maKH, String tenKH, Date ngaySinh, String diaChi, String sDT, String cCCD,
+			boolean gioiTinh) {
+		super();
+		this.maKH = maKH;
+		this.tenKH = tenKH;
+		this.ngaySinh = ngaySinh;
+		this.diaChi = diaChi;
+		this.sDT = sDT;
+		this.cCCD = cCCD;
+		this.gioiTinh = gioiTinh;
+	}
+	public KhachHang(String maKH) {
+		super();
+		this.maKH = maKH;
+	}
+	public KhachHang() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(maKH);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KhachHang other = (KhachHang) obj;
+		return Objects.equals(maKH, other.maKH);
+	}
+	@Override
+	public String toString() {
+		return "KhachHang [maKH=" + maKH + ", tenKH=" + tenKH + ", ngaySinh=" + ngaySinh + ", diaChi=" + diaChi
+				+ ", sDT=" + sDT + ", cCCD=" + cCCD + ", gioiTinh=" + gioiTinh + "]";
+	}
+	
+	
+	
+}
